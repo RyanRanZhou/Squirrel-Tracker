e85fe3588659685bc309886e47b498174461f569
sjjin1123 <81499058+sjjin1123@users.noreply.github.com>
2021-04-08 01:43:33 +0800

    Update urls.py

--- tracker/core/urls.py
+++ tracker/core/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-
 from .views import MapView, ListView, StatsView
-
 urlpatterns = [
     path('map/', MapView.as_view()),
     path('sightings/', ListView.as_view()),
-    path('sightings/stats', StatsView.as_view())
+    path('sightings/', StatsView.as_view())
 ]
