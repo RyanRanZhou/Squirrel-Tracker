985f1a9e228638f13781f9ff81ca52886df13f83
Ran Zhou <rz2527@columbia.com>
2021-04-11 10:37:30 +0000

    Fixed the problem for merge

--- tracker/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Sighting
-
-# Register your models here.
-@admin.register(Sighting)
-class SightingAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Sighting._meta.get_fields()]
--- tracker/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import MapView, ListView, StatsView
-urlpatterns = [
-    path('map/', MapView.as_view()),
-    path('sightings/', ListView.as_view()),
-    path('sightings/stats', StatsView.as_view())
-]
